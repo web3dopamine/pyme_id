ed58328d27388c52ea74e797796a896ac84a75cb
TahaKPMGBlockchain <dhaileytaha@gmail.com>
2023-04-27 11:44:25 +0800

    initial

---
+++ pages/App.tsx
@@ -0,0 +1,300 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-shadow */
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import { MetamaskAdapter } from "@web3auth/metamask-adapter";
+import { Web3Auth } from "@web3auth/modal";
+import { TorusWalletAdapter } from "@web3auth/torus-evm-adapter";
+// import RPC from ".api/ethersRPC"; // for using ethers.js
+// Plugins
+import { TorusWalletConnectorPlugin } from "@web3auth/torus-wallet-connector-plugin";
+// Adapters
+import { WalletConnectV1Adapter } from "@web3auth/wallet-connect-v1-adapter";
+import { useEffect, useState } from "react";
+import RPC from "./api/web3RPC"; // for using web3.js
+
+
+const clientId = "BGyqdNx0dqfAmAP1U2TBuEvAVKPj0Tz8cvKnTZa0BcaTUT61ocdaywLJ4UE1Jk6YMaHVVfCtC6a3J90KyoZjthc"; // get from https://dashboard.web3auth.io
+
+function App() {
+  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
+  const [torusPlugin, setTorusPlugin] = useState<TorusWalletConnectorPlugin | null>(null);
+  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(null);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3Auth({
+          clientId,
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0x1",
+            rpcTarget: "https://rpc.ankr.com/eth", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+          },
+          web3AuthNetwork: "cyan",
+        });
+
+        // plugins and adapters are optional and can be added as per your requirement
+        // read more about plugins here: https://web3auth.io/docs/sdk/web/plugins/
+
+        // adding torus wallet connector plugin
+
+        const torusPlugin = new TorusWalletConnectorPlugin({
+          torusWalletOpts: {},
+          walletInitOptions: {
+            whiteLabel: {
+              theme: { isDark: true, colors: { primary: "#00a8ff" } },
+              logoDark: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+              logoLight: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+            },
+            useWalletConnect: true,
+            enableLogging: true,
+          },
+        });
+        setTorusPlugin(torusPlugin);
+        await web3auth.addPlugin(torusPlugin);
+
+        // read more about adapters here: https://web3auth.io/docs/sdk/web/adapters/
+
+        // adding wallet connect v1 adapter
+
+        const walletConnectV1Adapter = new WalletConnectV1Adapter({
+          adapterSettings: {
+            bridge: "https://bridge.walletconnect.org",
+          },
+          clientId,
+        });
+
+        web3auth.configureAdapter(walletConnectV1Adapter);
+
+        // adding metamask adapter
+
+        const metamaskAdapter = new MetamaskAdapter({
+          clientId,
+          sessionTime: 3600, // 1 hour in seconds
+          web3AuthNetwork: "cyan",
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0x1",
+            rpcTarget: "https://rpc.ankr.com/eth", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+          },
+        });
+        // we can change the above settings using this function
+        metamaskAdapter.setAdapterSettings({
+          sessionTime: 86400, // 1 day in seconds
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0x1",
+            rpcTarget: "https://rpc.ankr.com/eth", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+          },
+          web3AuthNetwork: "cyan",
+        });
+
+        // it will add/update  the metamask adapter in to web3auth class
+        web3auth.configureAdapter(metamaskAdapter);
+
+        const torusWalletAdapter = new TorusWalletAdapter({
+          clientId,
+        });
+
+        // it will add/update  the torus-evm adapter in to web3auth class
+        web3auth.configureAdapter(torusWalletAdapter);
+
+        setWeb3auth(web3auth);
+
+        await web3auth.initModal();
+        if (web3auth.provider) {
+          setProvider(web3auth.provider);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const login = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const web3authProvider = await web3auth.connect();
+    setProvider(web3authProvider);
+    uiConsole("Logged in Successfully!");
+  };
+
+  const authenticateUser = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const idToken = await web3auth.authenticateUser();
+    uiConsole(idToken);
+  };
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const user = await web3auth.getUserInfo();
+    uiConsole(user);
+  };
+
+  const logout = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    await web3auth.logout();
+    setProvider(null);
+  };
+
+  const showWCM = async () => {
+    if (!torusPlugin) {
+      uiConsole("torus plugin not initialized yet");
+      return;
+    }
+    torusPlugin.showWalletConnectScanner();
+    uiConsole();
+  };
+
+  const initiateTopUp = async () => {
+    if (!torusPlugin) {
+      uiConsole("torus plugin not initialized yet");
+      return;
+    }
+    torusPlugin.initiateTopup("moonpay", {
+      selectedAddress: "0x8cFa648eBfD5736127BbaBd1d3cAe221B45AB9AF",
+      selectedCurrency: "USD",
+      fiatValue: 100,
+      selectedCryptoCurrency: "ETH",
+      chainNetwork: "mainnet",
+    });
+  };
+
+  const getChainId = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const chainId = await rpc.getChainId();
+    uiConsole(chainId);
+  };
+
+  const addChain = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const newChain = {
+      chainId: "0x5",
+      displayName: "Goerli",
+      chainNamespace: CHAIN_NAMESPACES.EIP155,
+      tickerName: "Goerli",
+      ticker: "ETH",
+      decimals: 18,
+      rpcTarget: "https://rpc.ankr.com/eth_goerli",
+      blockExplorer: "https://goerli.etherscan.io",
+    };
+    await web3auth?.addChain(newChain);
+    uiConsole("New Chain Added");
+  };
+
+  const switchChain = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    await web3auth?.switchChain({ chainId: "0x5" });
+    uiConsole("Chain Switched");
+  };
+
+  const getAccounts = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const address = await rpc.getAccounts();
+    uiConsole(address);
+  };
+
+  const getBalance = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const balance = await rpc.getBalance();
+    uiConsole(balance);
+  };
+
+  const sendTransaction = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const receipt = await rpc.sendTransaction();
+    uiConsole(receipt);
+  };
+
+  const signMessage = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const signedMessage = await rpc.signMessage();
+    uiConsole(signedMessage);
+  };
+
+  const getPrivateKey = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const privateKey = await rpc.getPrivateKey();
+    uiConsole(privateKey);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function uiConsole(...args: any[]): void {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+  }
+
+  const loggedInView = (
+   <>
+   </>
+  );
+    
+  
+  const unloggedInView = (
+    <button onClick={login} className="card">
+      Login
+    </button>
+  );
+
+  return (
+    <div className="container">
+      <h1 className="title">
+        Welcome to Pyme Id
+      </h1>
+
+      <div className="grid">{provider ? loggedInView : unloggedInView}</div>
+
+      <footer className="footer">
+       
+      </footer>
+    </div>
+  );
+}
+
+export default App;
